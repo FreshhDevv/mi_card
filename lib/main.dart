27d89695110f0c596fc064308804f75f57b1ab56
FreshDev <billyhans90@gmail.com>
2022-07-03 16:00:01 +0100

    Laravel developer

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Hans Weno',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'LARAVEL DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Color.fromARGB(255, 174, 230, 224),
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               )
